3cbc6e7fe94f20ded6e1afd060e440e1fc6d0503
SyawalinaShukri <syawalina.shukri@gmail.com>
2022-02-04 00:40:00 +0800

    Create .gitattributes

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Academic UiTM Calendar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">UiTM HEA Calendar</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li><a href="schedule.php">UiTM Event</a></li>
+      <li><a href="selected_events_client.php">My Schedule</a></li>
+    </ul>
+  </div>
+</nav>
+
+</body>
+
